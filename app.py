abcc67a17a77139ea83a96bd981d52b82682c12d
G. Aswin <aswin.g.rns@gmail.com>
2022-01-15 22:26:10 +0530

    Minor commenting mistake:

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def after_request(response):
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-# session = Session(app)
+session = Session(app)
 
 
 
